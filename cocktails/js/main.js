b318978df1a851062178076c9937657cf0974369
Andreas Fleischer <dev.fleischer@gmail.com>
2022-11-08 15:19:33 +0100

    updated cocktails

--- cocktails/js/main.js
+++ cocktails/js/main.js
@@ -1,3 +1,5 @@
+// Scroll up on page load
+
 if (history.scrollRestoration) {
   history.scrollRestoration = 'manual';
 } else {
@@ -6,13 +8,35 @@ if (history.scrollRestoration) {
   }
 }
 
-document.querySelector("a").addEventListener("click", getDrink)
-// document.querySelector('#next').addEventListener("click", nextDrink)
+// Eventlisteners
+
+document.querySelector("a").addEventListener("click", function(){
+  createSpace()
+  getDrink();
+})
+
 document.querySelector('#searchbar').addEventListener('keypress', function (e) {
   if (e.key === 'Enter') {
+    e.preventDefault()
+    createSpace()
     getDrink()
+    console.log("ok")
+    
   }
 });
+document.querySelector('#next').addEventListener("click", nextDrink)
+
+
+// Functions 
+
+function createSpace(){
+  document.querySelector('#cocktails').style.height = '100%'
+  document.querySelector('h2').style.margin = '1em'
+  let elmnt = document.querySelector("#cocktails");
+          elmnt.scrollIntoView();
+}
+
+
 
 function getDrink(){
   document.querySelector('ul').innerHTML = ''
@@ -44,6 +68,7 @@ function getDrink(){
   });
 }
 
+
 // function nextDrink(){
 //   counter += 1;
 //   let drink = document.querySelector('input').value
@@ -54,13 +79,20 @@ function getDrink(){
 //     document.querySelector('img').src = data.drinks[counter].strDrinkThumb
 //     document.querySelector('h2').innerHTML = data.drinks[counter].strDrink
 //     document.querySelector('h3').innerHTML = data.drinks[counter].strInstructions
-//     document.querySelector('#ingredient1').innerHTML = data.drinks[counter].strIngredient1
-//     document.querySelector('#ingredient2').innerHTML = data.drinks[counter].strIngredient2
-//     document.querySelector('#ingredient3').innerHTML = data.drinks[counter].strIngredient3
-//     document.querySelector('#ingredient4').innerHTML = data.drinks[counter].strIngredient4
-//     document.querySelector('#ingredient5').innerHTML = data.drinks[counter].strIngredient5
-//     document.querySelector('#ingredient6').innerHTML = data.drinks[counter].strIngredient6
-//     document.querySelector('#ingredient7').innerHTML = data.drinks[counter].strIngredient7
-//     document.querySelector('#ingredient8').innerHTML = data.drinks[counter].strIngredient8
+//     let obj = data.drinks[counter]
+//     let ingredients = 1
+//     for(let el in obj){
+//       if(obj[`strIngredient${ingredients}`]){
+//         const li = document.createElement('li')
+//         if(obj[`strMeasure${ingredients}`]){
+//           li.textContent = obj[`strMeasure${ingredients}`] + " " + obj[`strIngredient${ingredients}`]
+//         }else li.textContent = obj[`strIngredient${ingredients}`]
+        
+//         document.querySelector('ul').appendChild(li)
+//         ingredients += 1  
+//       }
+      
+//     }
+//     console.log(data.drinks[counter][`strIngredient${ingredients}`])
 //   });
 // }
